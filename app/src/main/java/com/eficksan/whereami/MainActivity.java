28d4c6b3b3983be2e300a4c082530043d4fcf4de
Aleksei Ivshin <ivshinalekseim@mail.ru>
2016-04-26 00:24:20 +0400

    WAI-2: Added basic functional

--- app/src/main/java/com/eficksan/whereami/MainActivity.java
+++ app/src/main/java/com/eficksan/whereami/MainActivity.java
@@ -1,8 +1,10 @@
 package com.eficksan.whereami;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import com.eficksan.whereami.fragments.LocationRequestingFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, LocationRequestingFragment.newInstance(), LocationRequestingFragment.TAG)
+                    .addToBackStack(LocationRequestingFragment.TAG)
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/eficksan/whereami/fragments/LocationRequestingFragment.java
+++ app/src/main/java/com/eficksan/whereami/fragments/LocationRequestingFragment.java
@@ -1,39 +1,176 @@
 package com.eficksan.whereami.fragments;
 
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.eficksan.whereami.App;
 import com.eficksan.whereami.R;
+
+import javax.inject.Inject;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static android.support.v4.content.ContextCompat.checkSelfPermission;
+
 /**
  * Created by Aleksei Ivshin
  * on 24.04.2016.
  */
-public class LocationRequestingFragment extends Fragment{
+@SuppressWarnings("ResourceType")
+public class LocationRequestingFragment extends Fragment implements LocationListener {
 
-    @Bind(R.id.location_label)
+    public static final int LOCATION_PERMISION_REQUEST = 42;
+
+    public static final String TAG = LocationRequestingFragment.class.getSimpleName();
+
+    @Bind(R.id.label_location)
     TextView mLocationLabel;
+    @Bind(R.id.label_latitude)
+    TextView mLatitudeLabel;
+    @Bind(R.id.label_longitude)
+    TextView mLongitudeLabel;
 
+    @Inject
+    LocationManager locationManager;
+
+    private String provider;
+
+    public static LocationRequestingFragment newInstance() {
+        return new LocationRequestingFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((App) getActivity().getApplication()).getObjectGraph().inject(this);
+
+        boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        Log.v(TAG, "GPS is " + (enabled ? "enabled" : "disabled"));
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Log.v(TAG, String.format("Best provider is '%s'", provider));
+        if (!checkPermissions()) {
+            Log.v(TAG, String.format("OnCreate: Permissions were not granted: %s, %s ", ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION));
+            if (shouldShowRequestPermissionRationale(ACCESS_COARSE_LOCATION)) {
+                //TODO: show explanation
+                Log.v(TAG, "Should show explanations");
+            } else {
+                Log.v(TAG, "Request permissions with request key = " + LOCATION_PERMISION_REQUEST);
+                requestPermissions(new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, LOCATION_PERMISION_REQUEST);
+            }
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_location_requesting, container, false);
         ButterKnife.bind(this, rootView);
+        if (checkPermissions()) {
+            Location location = locationManager.getLastKnownLocation(provider);
+            Log.v(TAG, "Last known location " + location);
+
+            // Initialize the location fields
+            if (location != null) {
+                Log.v(TAG, "Provider " + provider + " has been selected.");
+                onLocationChanged(location);
+            } else {
+                mLatitudeLabel.setText(R.string.location_not_available);
+                mLongitudeLabel.setText(R.string.location_not_available);
+            }
+        }
         return rootView;
+    }
+
+    /* Request updates at startup */
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (provider != null && checkPermissions()) {
+            Log.v(TAG, String.format("Request location updates provider = %s, time interval = %d, distance = %d", provider, 400, 1));
+            locationManager.requestLocationUpdates(provider, 1000, 1, this);
+        } else {
+            Log.v(TAG, String.format("OnResume: Permissions were not granted: %s, %s ", ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (provider != null && checkPermissions()) {
+            Log.v(TAG, "Stops Request location updates");
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    private boolean checkPermissions() {
+        return (checkSelfPermission(getActivity(), ACCESS_FINE_LOCATION) == PERMISSION_GRANTED
+                || checkSelfPermission(getActivity(), ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (LOCATION_PERMISION_REQUEST == requestCode) {
+            for (int grantResult : grantResults) {
+                if (PERMISSION_GRANTED == grantResult) {
+                    Toast.makeText(getActivity(), "Permissions granted!",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        }
     }
 
     @OnClick(R.id.request_location)
     public void handleRequestLocation() {
-        throw new RuntimeException("Not implemented yet!");
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.v(TAG, "Location changed");
+        int lat = (int) (location.getLatitude());
+        int lng = (int) (location.getLongitude());
+        mLatitudeLabel.setText(String.valueOf(lat));
+        mLongitudeLabel.setText(String.valueOf(lng));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Toast.makeText(getActivity(), "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Toast.makeText(getActivity(), "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 }
