b0f1e39d96cff7014629db48fb69099c7bf84916
Aleksei Ivshin <ivshinalekseim@mail.ru>
2016-04-25 21:35:43 +0400

    WAI-1: Added handler stub

--- app/src/main/java/com/eficksan/whereami/fragments/LocationRequestingFragment.java
+++ app/src/main/java/com/eficksan/whereami/fragments/LocationRequestingFragment.java
@@ -6,10 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.eficksan.whereami.R;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Aleksei Ivshin
@@ -17,11 +20,20 @@ import butterknife.ButterKnife;
  */
 public class LocationRequestingFragment extends Fragment{
 
+    @Bind(R.id.location_label)
+    TextView mLocationLabel;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_location_requesting, container, false);
         ButterKnife.bind(this, rootView);
         return rootView;
+    }
+
+    @OnClick(R.id.request_location)
+    public void handleRequestLocation() {
+        throw new RuntimeException("Not implemented yet!");
     }
 }
