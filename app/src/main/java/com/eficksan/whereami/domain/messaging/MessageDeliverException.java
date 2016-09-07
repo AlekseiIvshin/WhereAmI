1fc06dfcf4987a8cfceee1b0b10209404b172110
Aleksei Ivshin <eficksan@gmail.com>
2016-09-07 21:47:21 +0400

    Refactoring

--- app/src/main/java/com/eficksan/whereami/domain/messaging/MessageDeliverException.java
+++
@@ -1,9 +0,0 @@
-package com.eficksan.whereami.domain.messaging;
-
-/**
- * Created by Aleksei Ivshin
- * on 25.08.2016.
- */
-public class MessageDeliverException extends Exception {
-
-}
--- app/src/main/java/com/eficksan/whereami/ioc/messaging/MessagingModule.java
+++ app/src/main/java/com/eficksan/whereami/ioc/messaging/MessagingModule.java
@@ -1,10 +1,6 @@
 package com.eficksan.whereami.ioc.messaging;
 
-import android.app.Activity;
-
-import com.eficksan.whereami.data.messaging.FirebaseDatabaseMessagesRepository;
 import com.eficksan.whereami.data.messaging.MessagesRepository;
-import com.eficksan.whereami.domain.messaging.PlaceMessageInteractor;
 import com.eficksan.whereami.domain.messaging.PlaceMessageValidator;
 import com.eficksan.whereami.domain.messaging.PlacingMessageInteractor;
 import com.eficksan.whereami.ioc.fragments.FragmentScope;
@@ -23,11 +19,6 @@ public class MessagingModule {
     @Provides
     public PlaceMessageValidator providePlaceMessageValidator() {
         return new PlaceMessageValidator();
-    }
-
-    @Provides
-    public PlaceMessageInteractor provideMessagingInteractor(Activity activity) {
-        return new PlaceMessageInteractor(activity);
     }
 
     @Provides
